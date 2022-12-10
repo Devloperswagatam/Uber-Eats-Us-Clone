8df435086a518db10fa43e448262debf04ae62a9
Devloperswagatam <118425263+Devloperswagatam@users.noreply.github.com>
2022-12-11 01:14:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+function changeBg(){
+    // let navbar = document.getElementById('navbar');
+    let navbar = document.querySelector('.transparent');
+    let scrollValue = window.scrollY;
+    if(scrollValue < 100){
+        navbar.classList.remove('white');
+        navbar.style.borderBottom = 'none';
+    }else{
+        navbar.classList.add('white');
+        navbar.style.borderBottom = '1px solid lightgrey';
+    }
+}
+window.addEventListener('scroll', changeBg);
+
+// END OF NAVBAR BACKGROUND COLOR
+
+function viewAdress(){
+    let address = document.querySelector('.address');
+    let scrollValue = window.scrollY;
+    if(scrollValue < 400){
+        address.style.display = 'none';
+    }else{
+        address.style.display = 'block';
+    }
+}
+window.addEventListener('scroll', viewAdress);
+
+// SIDE BAR OPEN AND CLOSE
+const menuBtn = document.querySelector('#menu');
+const sidebar = document.querySelector('.sidebar');
+const boxShadow = document.querySelector('.box-shadow');
+
+menuBtn.addEventListener('click', () => {
+    sidebar.style.left = '0';
+    boxShadow.style.display = 'block';
+})
+
+boxShadow.addEventListener('click', ()=> {
+    sidebar.style.left = '-300px';
+    boxShadow.style.display = 'none';
+})
+
